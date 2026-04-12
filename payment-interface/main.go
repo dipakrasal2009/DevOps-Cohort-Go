685552aa4853c8010ef17b7047e7ee6e2ae73428
dipakrasal2009 <dipakrasal2009@gmail.com>
2026-04-12 22:14:11 +0530

    use interface
    - add credit card method using the interface
    - interface is done

--- payment-interface/main.go
+++ payment-interface/main.go
@@ -5,6 +5,7 @@ import (
 
     "github.com/dipakrasal2009/DevOps-Cohort-Go/payment-interface/pkg"
     "github.com/dipakrasal2009/DevOps-Cohort-Go/payment-interface/pkg/upi"
+    "github.com/dipakrasal2009/DevOps-Cohort-Go/payment-interface/pkg/creditcard"
 )
 
 func Checkout(method pkg.PaymentMethod, amount float64) string {
@@ -14,19 +15,22 @@ func Checkout(method pkg.PaymentMethod, amount float64) string {
 func main() {
     fmt.Println("payment interface example")
 
+    // UPI Payment
     dipakUPI := upi.UPIPayment{
         UpiID: "dipak@okhdfc",
         App:   "Gpay",
     }
 
-    msg := Checkout(dipakUPI, 50000.55)
-    fmt.Printf("payment successful: %s\n", msg)
+    msg1 := Checkout(dipakUPI, 50.55)
+    fmt.Printf("UPI: %s\n", msg1)
 
-    yashrajUPI := upi.UPIPayment{
-      UpiID: "yash@okhdfc",
-      App:   "Gpay",
+    // Credit Card Payment
+    dipakCard := creditcard.CreditCardPayment{
+        CardNumber: "1234-5678-9012-3456",
+        ExpiryDate: "12/28",
+        CVV:        "123",
     }
-    msg1 := Checkout(yashrajUPI, 50)
-    fmt.Printf("payment successful: %s\n", msg1)
 
+    msg2 := Checkout(dipakCard, 1200.00)
+    fmt.Printf("Credit Card: %s\n", msg2)
 }
--- payment-interface/pkg/creditcard/credit.go
+++ payment-interface/pkg/creditcard/credit.go
@@ -2,13 +2,16 @@ package creditcard
 
 import "fmt"
 
-type CredirCardPayment struct {
-  CardNumber string
-  ExpiryDate string
-  CVV string
+type CreditCardPayment struct {
+    CardNumber string
+    ExpiryDate string
+    CVV        string
 }
 
-func (c CreditCardPayment) Pay(amount float64) string{
-  msg := fmt.Sprintf("credit card pament of %.2f successfully completed uisng %s \n",amount,c.CardNumber)
-  return msg
+func (c CreditCardPayment) Pay(amount float64) string {
+    return fmt.Sprintf(
+        "Credit card payment of %.2f successful using %s",
+        amount,
+        c.CardNumber,
+    )
 }
