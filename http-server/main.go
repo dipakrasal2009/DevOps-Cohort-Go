9a863407fcdb658afba307ca69f7c4d9e43bdc9d
dipakrasal2009 <dipakrasal2009@gmail.com>
2026-04-28 19:50:42 +0530

    updated http server code

--- http-server/main.go
+++ http-server/main.go
@@ -1,31 +1,59 @@
 package main
 
 import (
+	"bytes"
 	"fmt"
 	"io"
+	"log"
 	"net/http"
 )
 
 func homeHandler(w http.ResponseWriter, r *http.Request) {
+	defer r.Body.Close()
 
-	fmt.Println("==== Incoming request ==== ")
-	fmt.Println("method: ", r.Method)
-	fmt.Println("path: ", r.URL.Path)
-	fmt.Println("query: ", r.URL.RawQuery)
-	fmt.Println("headers: ", r.Header)
-	fmt.Println("content length: ", r.ContentLength)
-	fmt.Println("host: ", r.Host)
-	fmt.Println("body: ", r.Body)
-	// read the body
+	if r.Method != http.MethodGet {
+		w.WriteHeader(http.StatusMethodNotAllowed)
+		fmt.Fprintln(w, "Method not allowed", http.StatusMethodNotAllowed)
+		return
+	}
 	body, err := io.ReadAll(r.Body)
 	if err != nil {
 		fmt.Println("error reading body: ", err)
 		return
 	}
-	fmt.Println("body data: ", string(body))
+	r.Body = io.NopCloser(bytes.NewBuffer(body))
 
-	w.WriteHeader(400)
-	fmt.Fprintln(w, "Helo From http-server demo")
+	log.Println("request: ", r.Method, r.URL.Path, r.URL.RawQuery, r.Header, r.ContentLength, r.Host, r.Body)
+	log.Println("body data: ", string(body))
+
+	w.Header().Set("Content-Type", "text/plain")
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte("Hello from http-server demo"))
+
+	// fmt.Println("==== Incoming request ==== ")
+	// fmt.Println("method: ", r.Method)
+	// fmt.Println("path: ", r.URL.Path)
+	// fmt.Println("query: ", r.URL.RawQuery)
+	// fmt.Println("headers: ", r.Header)
+	// fmt.Println("content length: ", r.ContentLength)
+	// fmt.Println("host: ", r.Host)
+	// fmt.Println("body: ", r.Body)
+	// //add some more info about the request
+	// fmt.Println("remote address: ", r.RemoteAddr)
+	// fmt.Println("request URI: ", r.RequestURI)
+	// fmt.Println("protocol: ", r.Proto)
+	// fmt.Println("user agent: ", r.UserAgent())
+	// fmt.Println("referer: ", r.Referer())
+
+	// body, err := io.ReadAll(r.Body)
+	// if err != nil {
+	// 	fmt.Println("error reading body: ", err)
+	// 	return
+	// }
+	// fmt.Println("body data: ", string(body))
+
+	// w.WriteHeader(400)
+	// fmt.Fprintln(w, "Helo From http-server demo")
 }
 
 func main() {
@@ -33,6 +61,12 @@ func main() {
 
 	// registers
 	http.HandleFunc("/", homeHandler)
+
+	//handle errors
+	http.HandleFunc("/error", func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(http.StatusInternalServerError)
+		fmt.Fprintln(w, "Internal Server Error")
+	})
 
 	fmt.Println("Server running on localhost:8080")
 	http.ListenAndServe(":8080", nil)
